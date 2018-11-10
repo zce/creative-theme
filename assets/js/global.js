58e5ac549272faffdefb14837b4a6ae590cbf760
zce <w@zce.me>
2018-11-10 22:27:01 +0800

    :tada: Initial commit

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,85 +1,83 @@
-/**
- * Global scripts
- */
+const qs = document.querySelector.bind(document)
+const qsa = document.querySelectorAll.bind(document)
 
-(function (window) {
-  const { document, location } = window
-  const qs = document.querySelector.bind(document)
-  const qsa = document.querySelectorAll.bind(document)
+function loadStyle (href) {
+  const linkElement = document.createElement('link')
+  linkElement.rel = 'stylesheet'
+  linkElement.type = 'text/css'
+  linkElement.href = href
+  document.head.appendChild(linkElement)
+}
 
-  function loadStyle (href) {
-    const linkElement = document.createElement('link')
-    linkElement.rel = 'stylesheet'
-    linkElement.type = 'text/css'
-    linkElement.href = href
-    document.head.appendChild(linkElement)
-  }
+function loadScript (src, callback) {
+  const scriptElement = document.createElement('script')
+  scriptElement.src = src
+  callback && scriptElement.addEventListener('load', callback)
+  document.body.appendChild(scriptElement)
+}
 
-  function loadScript (src, callback) {
-    const scriptElement = document.createElement('script')
-    scriptElement.src = src
-    callback && scriptElement.addEventListener('load', callback)
-    document.body.appendChild(scriptElement)
-  }
+// search
+// -----------------------------------------------------------------------------
+;(() => {
+  const input = qs('#search_input')
+  if (!input) return false
 
-  // // cover
-  // qs('.site-cover') || document.body.classList.add('no-cover')
+  loadScript('https://unpkg.com/ghost-search/dist/ghost-search.js', () => {
+    const result = document.createElement('div')
+    result.id = 'search_result'
+    result.className = 'dropdown-menu dropdown-menu-right'
+    result.innerHTML = `<span class="dropdown-item disabled">${input.placeholder}...</span>`
+    input.parentElement.classList.add('dropdown')
+    input.parentElement.appendChild(result)
 
-  // search
-  const searchElement = qs('#search_input')
-  if (searchElement) {
-    loadScript('https://unpkg.com/ghost-search/dist/ghost-search.js', () => {
-      const resultElement = document.createElement('div')
-      resultElement.id = 'search_result'
-      resultElement.className = 'dropdown-menu dropdown-menu-right'
-      resultElement.innerHTML = `<span class="dropdown-item disabled">${searchElement.placeholder}...</span>`
-      searchElement.parentElement.classList.add('dropdown')
-      searchElement.parentElement.appendChild(resultElement)
-
-      /* eslint-disable no-new */
-      new window.GhostSearch({
-        input: '#search_input',
-        results: '#search_result',
-        template: item => `<a class="dropdown-item" href="${item.url}">${item.title}</a>`,
-        api: {
-          parameters: {
-            fields: ['url', 'title']
-          }
-        },
-        on: {
-          afterDisplay: results => {
-            if (results.total !== 0) return false
-            if (!searchElement.value) return false
-            resultElement.innerHTML = `<span class="dropdown-item disabled">No results</span>`
-          }
+    /* eslint-disable no-new */
+    new window.GhostSearch({
+      input: '#search_input',
+      results: '#search_result',
+      template: i => `<a class="dropdown-item" href="${i.url}">${i.title}</a>`,
+      api: {
+        parameters: { fields: ['url', 'title'] }
+      },
+      on: {
+        afterDisplay: results => {
+          if (results.total !== 0) return false
+          if (!input.value) return false
+          result.innerHTML = `<span class="dropdown-item disabled">No results</span>`
         }
-      })
+      }
     })
-  }
+  })
+})()
 
-  // lazyload
-  if (qs('.post-card [data-src]')) {
-    loadScript('https://unpkg.com/vanilla-lazyload', () => {
-      window.lazyloader = new window.LazyLoad({
-        elements_selector: '.post-card [data-src]'
-      })
+// img lazyload
+// -----------------------------------------------------------------------------
+;(() => {
+  if (!qs('.post-card [data-src]')) return false
+
+  loadScript('https://unpkg.com/vanilla-lazyload', () => {
+    window.lazyloader = new window.LazyLoad({
+      elements_selector: '.post-card [data-src]'
     })
-  }
+  })
+})()
 
-  // prismjs loader
-  const codeElements = qsa('.post-content pre code')
-  if (codeElements.length) {
-    // code card polyfill
-    codeElements.forEach(item => {
-      if (item.classList.length) return
-      item.classList.add('language-basic')
-    })
+// highlight prismjs
+// -----------------------------------------------------------------------------
+;(() => {
+  const elements = qsa('.post-content pre code')
+  if (!elements.length) return false
+  // code card polyfill
+  elements.forEach(item => {
+    item.classList.length || item.classList.add('language-basic')
+  })
 
-    loadStyle('https://unpkg.com/prismjs/themes/prism-okaidia.css')
-    loadScript('https://unpkg.com/prismjs/prism.js')
-  }
+  loadStyle('https://unpkg.com/prismjs/themes/prism-okaidia.css')
+  loadScript('https://unpkg.com/prismjs/prism.js')
+})()
 
-  // gallery flex
+// gallery
+// -----------------------------------------------------------------------------
+;(() => {
   qsa('.kg-gallery-image img').forEach(item => {
     const container = item.closest('.kg-gallery-image')
     const width = item.attributes.width.value
@@ -88,37 +86,42 @@
     container.style.flex = ratio + ' 1 0%'
   })
 
-  // gallery preview
-  const galleryElements = qsa('.kg-gallery-container')
-  if (galleryElements.length) {
-    loadStyle('https://unpkg.com/lightgallery.js/dist/css/lightgallery.min.css')
-    loadScript('https://unpkg.com/lightgallery.js/dist/js/lightgallery.js', () => {
-      galleryElements.forEach(item => {
-        item.querySelectorAll('.kg-gallery-image').forEach(sub => {
-          sub.dataset.src = sub.children[0].src
-        })
-        window.lightGallery(item, { selector: '.kg-gallery-image' })
+  // preview
+  const elements = qsa('.kg-gallery-container')
+  if (!elements.length) return false
+  loadStyle('https://unpkg.com/lightgallery.js/dist/css/lightgallery.min.css')
+  loadScript('https://unpkg.com/lightgallery.js/dist/js/lightgallery.js', () => {
+    elements.forEach(item => {
+      item.querySelectorAll('.kg-gallery-image').forEach(sub => {
+        sub.dataset.src = sub.children[0].src
       })
+      window.lightGallery(item, { selector: '.kg-gallery-image' })
     })
-  }
+  })
+})()
 
-  // post share
+// TODO: post share
+// -----------------------------------------------------------------------------
+;(() => {
   qsa('.post-share a').forEach(item => item.addEventListener('click', e => {
-    window.open(this.href, 'share-window', 'width=640,height=360')
+    window.open(e.currentTarget.href, 'share-window', 'width=640,height=360')
     e.preventDefault()
   }))
+})()
 
-  // subscribe form hiddens
+// subscribe hidden form value
+// -----------------------------------------------------------------------------
+;(() => {
   qsa('input[name=location]').forEach(item => { item.value = item.value || location.href })
   qsa('input[name=referrer]').forEach(item => { item.value = item.value || document.referrer })
+})()
 
-  // site preloader
+// site preloader
+// -----------------------------------------------------------------------------
+;(() => {
   const loader = qs('.site-preloader')
   if (loader) {
-    // loader.addEventListener('transitionend', () => {
-    //   loader.parentElement.removeChild(loader)
-    // })
     loader.style.opacity = 0
     setTimeout(() => loader.parentElement.removeChild(loader), 1000)
   }
-})(window)
+})()
